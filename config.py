c7c653a4538155472e0ecd22a77b47499d0d5106
Jenny Hu <jennyh@advmicrogrid.com>
2018-07-10 16:35:53 -0700

    update to templates

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Harrison4@pytest1.cyaovfjv3p4h.us-west-2.rds.amazonaws.com/flaskdb'
+
 
 # Uncomment the line below if you want to work with a local DB
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
